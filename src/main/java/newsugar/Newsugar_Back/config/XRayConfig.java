cfe79681ca301d9b2cd6806b33e899f208ab99b4
KimHaeCho <jiyun5823@gmail.com>
2025-12-29 15:59:48 +0900

    Implement monitoring: X-Ray, Fluent Bit, CloudWatch Agent, and App config

---
+++ src/main/java/newsugar/Newsugar_Back/config/XRayConfig.java
@@ -0,0 +1,15 @@
+package newsugar.Newsugar_Back.config;
+
+import com.amazonaws.xray.javax.servlet.AWSXRayServletFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import jakarta.servlet.Filter;
+
+@Configuration
+public class XRayConfig {
+
+    @Bean
+    public Filter TracingFilter() {
+        return new AWSXRayServletFilter("Newsugar-Backend");
+    }
+}
