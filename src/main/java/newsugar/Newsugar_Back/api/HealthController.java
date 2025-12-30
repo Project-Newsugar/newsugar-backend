1e9adf4bdd0affef4f567cf009e58e3d832a2ef3
KimHaeCho <jiyun5823@gmail.com>
2025-12-30 16:13:47 +0900

    Fix: Add missing RequestMapping import in HealthController

--- src/main/java/newsugar/Newsugar_Back/api/HealthController.java
+++ src/main/java/newsugar/Newsugar_Back/api/HealthController.java
@@ -3,6 +3,7 @@ package newsugar.Newsugar_Back.api;
 import newsugar.Newsugar_Back.common.ApiResult;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
