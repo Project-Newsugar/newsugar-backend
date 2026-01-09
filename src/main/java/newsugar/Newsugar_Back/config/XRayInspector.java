23b0fcead3be48ea406ef188855c6410a83b338e
KimHaeCho <jiyun5823@gmail.com>
2026-01-09 14:45:46 +0900

    Fix X-Ray Inspector compilation error and improve safety

--- src/main/java/newsugar/Newsugar_Back/config/XRayInspector.java
+++ src/main/java/newsugar/Newsugar_Back/config/XRayInspector.java
@@ -1,26 +1,44 @@
 package newsugar.Newsugar_Back.config;
 
-import com.amazonaws.xray.spring.aop.AbstractXRayInterceptor;
 import com.amazonaws.xray.AWSXRay;
-import org.aopalliance.intercept.MethodInvocation;
+import com.amazonaws.xray.entities.Subsegment;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
-public class XRayInspector extends AbstractXRayInterceptor {
+public class XRayInspector {
 
-    @Override
     @Pointcut("execution(* newsugar.Newsugar_Back.domain..service..*(..)) || execution(* newsugar.Newsugar_Back.domain..repository..*(..))")
     public void xrayEnabledClasses() {}
 
-    @Override
-    public Object invoke(MethodInvocation invocation) throws Throwable {
-        // 현재 스레드에 X-Ray 세그먼트가 있을 때만 하위 세그먼트 생성 (스케줄러 등 컨텍스트 유실 시 에러 방지)
-        if (AWSXRay.getCurrentSegmentOptional().isPresent()) {
-            return super.invoke(invocation);
+    @Around("xrayEnabledClasses()")
+    public Object traceMethod(ProceedingJoinPoint pjp) throws Throwable {
+        // X-Ray 세그먼트가 없으면 추적하지 않음 (스케줄러 등 컨텍스트 유실 시 에러 방지)
+        if (!AWSXRay.getCurrentSegmentOptional().isPresent()) {
+            return pjp.proceed();
         }
-        return invocation.proceed();
+
+        // 서브세그먼트 생성
+        Subsegment subsegment = AWSXRay.beginSubsegment(pjp.getSignature().getName());
+        try {
+            if (subsegment != null) {
+                subsegment.putMetadata("Class", pjp.getTarget().getClass().getSimpleName());
+                subsegment.putMetadata("Method", pjp.getSignature().getName());
+            }
+            return pjp.proceed();
+        } catch (Exception e) {
+            if (subsegment != null) {
+                subsegment.addException(e);
+            }
+            throw e;
+        } finally {
+            if (subsegment != null) {
+                AWSXRay.endSubsegment();
+            }
+        }
     }
 }
