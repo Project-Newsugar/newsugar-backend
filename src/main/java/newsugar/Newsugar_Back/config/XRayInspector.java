3203039caf579f4dc4b14fcde4285e7570138ccc
KimHaeCho <jiyun5823@gmail.com>
2026-01-09 14:28:58 +0900

    fix: prevent X-Ray SegmentNotFoundException by checking current segment presence

--- src/main/java/newsugar/Newsugar_Back/config/XRayInspector.java
+++ src/main/java/newsugar/Newsugar_Back/config/XRayInspector.java
@@ -1,6 +1,8 @@
 package newsugar.Newsugar_Back.config;
 
 import com.amazonaws.xray.spring.aop.AbstractXRayInterceptor;
+import com.amazonaws.xray.AWSXRay;
+import org.aopalliance.intercept.MethodInvocation;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
@@ -12,4 +14,13 @@ public class XRayInspector extends AbstractXRayInterceptor {
     @Override
     @Pointcut("execution(* newsugar.Newsugar_Back.domain..service..*(..)) || execution(* newsugar.Newsugar_Back.domain..repository..*(..))")
     public void xrayEnabledClasses() {}
+
+    @Override
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        // 현재 스레드에 X-Ray 세그먼트가 있을 때만 하위 세그먼트 생성 (스케줄러 등 컨텍스트 유실 시 에러 방지)
+        if (AWSXRay.getCurrentSegmentOptional().isPresent()) {
+            return super.invoke(invocation);
+        }
+        return invocation.proceed();
+    }
 }
